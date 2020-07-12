3479a942fea332b1e1e781d7e1386cb339cc929f
mcraepv <61661042+mcraepv@users.noreply.github.com>
2020-07-12 16:03:59 -0500

    creates server logic

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const routes = require('./controllers/burgers-controller');
+const exphbs = require('express-handlebars');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(routes);
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.listen(PORT, function () {
+  console.log('Server listening on: http://localhost:' + PORT);
+});
