0063625ff502ce7ab093b03d848ffcba13764045
mcraepv <61661042+mcraepv@users.noreply.github.com>
2020-07-13 21:17:12 -0500

    removes console logs in script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,12 +2,10 @@ $('.eat-btn').on('click', function (event) {
   event.preventDefault();
 
   const id = $(this).data('id');
-  console.log(id);
 
   $.ajax(`/api/burgers/${id}`, {
     type: 'PUT',
   }).then(function () {
-    console.log('Burger eaten.');
     location.reload();
   });
 });
@@ -15,14 +13,12 @@ $('.eat-btn').on('click', function (event) {
 $('#add-btn').on('click', function (event) {
   event.preventDefault();
   const data = $('#burger-input').val();
-  console.log(data);
   if (!data) return;
 
   $.ajax('/api/burgers', {
     type: 'POST',
     data: { burgerName: data },
   }).then(function () {
-    console.log('Burger added!');
     location.reload();
   });
 });
@@ -31,12 +27,10 @@ $('.delete-btn').on('click', function (event) {
   event.preventDefault();
 
   const id = $(this).data('id');
-  console.log(id);
 
   $.ajax(`/api/burgers/${id}`, {
     type: 'DELETE',
   }).then(function () {
-    console.log('Burger deleted.');
     location.reload();
   });
 });
